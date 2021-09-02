bcd74d21973b981b746d6ca86550bf304282e8e5
luisrrlealmx <luisrrleal@gmail.com>
2021-09-02 10:18:35 -0500

    Primera versión 02/septiembre/2021

---
+++ script.js
@@ -0,0 +1,17 @@
+let html = document.getElementById("tiempo");
+
+setInterval(function(){
+    tiempo = new Date();
+
+    horas = tiempo.getHours();
+    minutos = tiempo.getMinutes();
+    segundos = tiempo.getSeconds();
+
+    if (horas<10)
+        horas = "0"+horas;
+    if (minutos<10)
+        segundos = "0"+segundos;
+    if (segundos<10)
+        segundos = "0"+segundos;
+    html.innerHTML = horas+" : "+minutos+" : "+segundos
+})
